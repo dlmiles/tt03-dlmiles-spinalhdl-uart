a612affd3e330148ca054787ceddddd3be4e5457
Darryl L. Miles <darryl.miles@darrylmiles.org>
2023-04-23 11:41:31 +0100

    src/** WIP

--- src/clock_stretch_invert_mux.v
+++ src/clock_stretch_invert_mux.v
@@ -11,7 +11,7 @@
 //	
 //
 module clock_stretch_invert_mux (
-    output reg                          clock_out,
+    output                              clock_out,
     input				clock_in,
     input				sel
 );
