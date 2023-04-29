233ac342ced0d1d709640880122879861892d0c0
Darryl L. Miles <darryl.miles@darrylmiles.org>
2023-04-29 11:16:05 +0100

    src/** WIP

--- src/uart_dummy.v
+++ src/uart_dummy.v
@@ -46,17 +46,25 @@ module uart_dummy (
     
     always @ (posedge clk) begin
         if (reset) begin
-            out8 <= 8'h0;
-            run <= 1;
+            out8[6:1] <= 6'h0;
+            out8[7] <= reset;
+            out8[0] <= io_resetCommandStrobe;
+            run <= 1'b1;
             count <= 8'h0;
         end else if (io_in7[6] && io_in7[5] && has_cmd) begin
-            out8 <= 8'b10101100;
-            run <= 1;
-            count <= 8'h11100111;
+            out8[6:1] <= 6'b010110;
+            out8[7] <= reset;
+            out8[0] <= io_resetCommandStrobe;
+            run <= 1'b1;
+            count <= 8'b11100111;
         end else if (count == 8'h0) begin
             out8[6:2] <= out8[6:2] + 1;
+            out8[7] <= reset;
+            out8[0] <= io_resetCommandStrobe;
             count <= count - 1;
         end else begin
+            out8[7] <= reset;
+            out8[0] <= io_resetCommandStrobe;
             count <= count - 1;
         end
     end
