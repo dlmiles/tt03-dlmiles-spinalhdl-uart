1daea32608fde27bead7d31615f5e8b1f19dd496
Darryl L. Miles <darryl.miles@darrylmiles.org>
2023-04-29 11:16:05 +0100

    src/** WIP

--- src/uart_dummy.v
+++ src/uart_dummy.v
@@ -44,9 +44,6 @@ module uart_dummy (
     reg [7:0] count;
     reg run;
     
-//    wire [4:0] out8_count;
-//    assign out8_count[4:0] = out8[6:2];
-
     always @ (posedge clk) begin
         if (reset) begin
             out8 <= 8'h0;
@@ -55,9 +52,11 @@ module uart_dummy (
         //end else if (io_in7[6] && io_in7[5] && cmd == CMD_CONFIG) begin
         end else if (io_in7[6] && io_in7[5] && has_cmd) begin
             out8 <= 8'b10101100;
+            run <= 1;
+            count <= 8'h0;
         end else if (count == 8'h0) begin
             out8[6:2] <= out8[6:2] + 1;
-            //out8_count <= out8_count + 1;
+            count <= count - 1;
         end else begin
             count <= count - 1;
         end
